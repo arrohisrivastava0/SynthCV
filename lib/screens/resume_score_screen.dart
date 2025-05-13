d47ba02c332da61889dbd8ead6ce5f04ced5174e
arrohisrivastava <arrohisrivastava0@gmail.com>
2025-05-14 01:18:59 +0530

    Fixed dashboard

--- lib/screens/resume_score_screen.dart
+++ lib/screens/resume_score_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:synthcv/widget/ats_analysis_widgets/visual_score.dart';
@@ -11,17 +10,31 @@ class ResumeScoreScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final double resumeScore = score?['resume_score'] ?? 77.6;
-    final List strengths = score?['strengths'] ?? [{"area": 'hehe', "comment": 'hello niggas how are you ifne sankyu', "score_impact":'+10'}];
-    final List areasOfImprovement = score?['areas_of_improvement'] ?? [{"area": 'hehe', "comment": 'hello niggas how are you ifne sankyu', "score_impact":'-9'}];
-    final String overallSummary = score?['overall_summary'] ?? "Your resume demonstrates a strong technical foundation for this Flutter Development internship. You clearly showcase experience with key required skills like Flutter, Firebase, Dart, REST API, and MVC through your projects and skills list. Your project work is directly relevant. To elevate your application further, you'll want to more explicitly align your resume with the 'Other Requirements' of the role, particularly by highlighting your interest in startups and your passion for Flutter development.";
-    final List<String> keyRecommendations =
-        score?['key_recommendations'].cast<String>() ??
-    [
-      "Add a resume summary mentioning 'startup interest', 'passion for Flutter', 'clean code'.",
-      "Use cover letter to explicitly confirm all eligibility (dates, WFH, duration) and 'other requirements'.",
-      "Subtly add phrases for 'ownership' or 'learning' in project details.",
-      "Ensure 'Mobile Application Development' is clear in skills."
-    ];
+    final List strengths = score?['strengths'] ??
+        [
+          {
+            "area": 'hehe',
+            "comment": 'hello niggas how are you ifne sankyu',
+            "score_impact": '+10'
+          }
+        ];
+    final List areasOfImprovement = score?['areas_of_improvement'] ??
+        [
+          {
+            "area": 'hehe',
+            "comment": 'hello niggas how are you ifne sankyu',
+            "score_impact": '-9'
+          }
+        ];
+    final String overallSummary = score?['overall_summary'] ??
+        "Your resume demonstrates a strong technical foundation for this Flutter Development internship. You clearly showcase experience with key required skills like Flutter, Firebase, Dart, REST API, and MVC through your projects and skills list. Your project work is directly relevant. To elevate your application further, you'll want to more explicitly align your resume with the 'Other Requirements' of the role, particularly by highlighting your interest in startups and your passion for Flutter development.";
+    final List<String> keyRecommendations = score?['key_recommendations'] ??
+        [
+          "Add a resume summary mentioning 'startup interest', 'passion for Flutter', 'clean code'.",
+          "Use cover letter to explicitly confirm all eligibility (dates, WFH, duration) and 'other requirements'.",
+          "Subtly add phrases for 'ownership' or 'learning' in project details.",
+          "Ensure 'Mobile Application Development' is clear in skills."
+        ];
 
     return Scaffold(
         appBar: AppBar(
@@ -50,7 +63,7 @@ class ResumeScoreScreen extends StatelessWidget {
               children: [
                 VisualScoreRow(
                   label: "Resume Score",
-                  percent: resumeScore,
+                  percent: resumeScore / 100,
                   color: const Color(0xFFC251D7),
                 ),
                 const SizedBox(height: 24),
@@ -73,7 +86,8 @@ class ResumeScoreScreen extends StatelessWidget {
                   decoration: BoxDecoration(
                     color: Colors.white.withOpacity(0.03),
                     borderRadius: BorderRadius.circular(12),
-                    border: Border.all(color: Colors.purpleAccent.withOpacity(0.3)),
+                    border:
+                        Border.all(color: Colors.purpleAccent.withOpacity(0.3)),
                     boxShadow: [
                       BoxShadow(
                         color: Colors.purpleAccent.withOpacity(0.05),
@@ -85,7 +99,8 @@ class ResumeScoreScreen extends StatelessWidget {
                   child: Row(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      const Icon(Icons.insights, color: Colors.purpleAccent, size: 20),
+                      const Icon(Icons.insights,
+                          color: Colors.purpleAccent, size: 20),
                       const SizedBox(width: 10),
                       Expanded(
                         child: Text(
@@ -113,66 +128,62 @@ class ResumeScoreScreen extends StatelessWidget {
                 ),
                 const SizedBox(height: 12),
                 ...strengths.map((item) => Container(
-                  margin: const EdgeInsets.symmetric(vertical: 8),
-                  padding: const EdgeInsets.all(14),
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.03),
-                    borderRadius: BorderRadius.circular(12),
-                    border: Border.all(
-                        color: Colors.greenAccent.withOpacity(0.3)),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.greenAccent.withOpacity(0.05),
-                        blurRadius: 4,
-                        offset: const Offset(0, 2),
+                      margin: const EdgeInsets.symmetric(vertical: 8),
+                      padding: const EdgeInsets.all(14),
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.03),
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                            color: Colors.greenAccent.withOpacity(0.3)),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.greenAccent.withOpacity(0.05),
+                            blurRadius: 4,
+                            offset: const Offset(0, 2),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Icon(Icons.check_circle,
-                          color: Colors.greenAccent, size: 18),
-                      const SizedBox(width: 6),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                Text(
-                                  item['area'] ?? '',
-                                  style: GoogleFonts.rubik(
-                                    color: Colors.white,
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              const Icon(Icons.check_circle,
+                                  color: Colors.greenAccent, size: 18),
+                              const SizedBox(width: 6),
+                              Text(
+                                item['area'] ?? '',
+                                style: GoogleFonts.rubik(
+                                  color: Colors.greenAccent,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
                                 ),
-                                Text(
-                                  item['score_impact'] ?? '',
-                                  style: GoogleFonts.rubik(
-                                    color: (item['score_impact'][0]=='+')?Colors.green:Colors.red,
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: 6),
-                            Text(
-                              item['comment'] ?? '',
-                              style: GoogleFonts.rubik(
-                                color: Colors.white,
-                                fontSize: 14,
                               ),
+                              const Spacer(),
+                              Text(
+                                item['score_impact'] ?? '',
+                                style: GoogleFonts.rubik(
+                                  color: (item['score_impact'][0] == '+')
+                                      ? Colors.green
+                                      : Colors.red,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 6),
+                          Text(
+                            item['comment'] ?? '',
+                            style: GoogleFonts.rubik(
+                              color: Colors.white,
+                              fontSize: 14,
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                )
-                ),
+                    )),
                 const SizedBox(height: 24),
                 Text(
                   "Areas of Improvement:",
@@ -187,66 +198,67 @@ class ResumeScoreScreen extends StatelessWidget {
                 ),
                 const SizedBox(height: 12),
                 ...areasOfImprovement.map((item) => Container(
-                  margin: const EdgeInsets.symmetric(vertical: 8),
-                  padding: const EdgeInsets.all(14),
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.03),
-                    borderRadius: BorderRadius.circular(12),
-                    border: Border.all(
-                        color: Colors.orangeAccent.withOpacity(0.3)),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.orangeAccent.withOpacity(0.05),
-                        blurRadius: 4,
-                        offset: const Offset(0, 2),
+                      margin: const EdgeInsets.symmetric(vertical: 8),
+                      padding: const EdgeInsets.all(14),
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.03),
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                            color: Colors.orangeAccent.withOpacity(0.3)),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.orangeAccent.withOpacity(0.05),
+                            blurRadius: 4,
+                            offset: const Offset(0, 2),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Icon(Icons.error_outline,
-                          color: Colors.redAccent, size: 18),
-                      const SizedBox(width: 6),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Icon(Icons.error_outline,
+                              color: Colors.redAccent, size: 18),
+                          const SizedBox(width: 6),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
+                                Row(
+                                  children: [
+                                    Text(
+                                      item['area'] ?? '',
+                                      style: GoogleFonts.rubik(
+                                        color: Colors.white,
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      item['score_impact'] ?? '',
+                                      style: GoogleFonts.rubik(
+                                        color: (item['score_impact'][0] == '+')
+                                            ? Colors.green
+                                            : Colors.red,
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 6),
                                 Text(
-                                  item['area'] ?? '',
+                                  item['comment'] ?? '',
                                   style: GoogleFonts.rubik(
                                     color: Colors.white,
                                     fontSize: 14,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  item['score_impact'] ?? '',
-                                  style: GoogleFonts.rubik(
-                                    color: (item['score_impact'][0]=='+')?Colors.green:Colors.red,
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.bold,
                                   ),
                                 ),
                               ],
                             ),
-                            const SizedBox(height: 6),
-                            Text(
-                              item['comment'] ?? '',
-                              style: GoogleFonts.rubik(
-                                color: Colors.white,
-                                fontSize: 14,
-                              ),
-                            ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                )
-                ),
+                    )),
                 Text(
                   "Suggestions to Improve:",
                   style: GoogleFonts.orbitron(
@@ -260,36 +272,35 @@ class ResumeScoreScreen extends StatelessWidget {
                 ),
                 const SizedBox(height: 20),
                 ...keyRecommendations.map((s) => Container(
-                  margin: const EdgeInsets.symmetric(vertical: 6),
-                  padding: const EdgeInsets.all(12),
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.04),
-                    borderRadius: BorderRadius.circular(12),
-                    border: Border.all(
-                        color: Colors.greenAccent.withOpacity(0.3)),
-                  ),
-                  child: Row(
-                    children: [
-                      const Icon(Icons.tips_and_updates,
-                          color: Colors.greenAccent, size: 20),
-                      const SizedBox(width: 10),
-                      Expanded(
-                        child: Text(
-                          s,
-                          style: GoogleFonts.rubik(
-                            color: Colors.white,
-                            fontSize: 14,
-                          ),
-                        ),
+                      margin: const EdgeInsets.symmetric(vertical: 6),
+                      padding: const EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.04),
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                            color: Colors.greenAccent.withOpacity(0.3)),
                       ),
-                    ],
-                  ),
-                )),
+                      child: Row(
+                        children: [
+                          const Icon(Icons.tips_and_updates,
+                              color: Colors.greenAccent, size: 20),
+                          const SizedBox(width: 10),
+                          Expanded(
+                            child: Text(
+                              s,
+                              style: GoogleFonts.rubik(
+                                color: Colors.white,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )),
                 const SizedBox(height: 30),
               ],
             ),
           ),
-        )
-    );
+        ));
   }
 }
